eed6f422265611c1ffe1023bb21da4ae1f25f9aa
dapcjunior <danielpereiracamposjunior@gmail.com>
2022-10-19 21:51:22 -0300

    Novo teste de comit

--- src/VariaveiseTiposPrimitivos.java
+++ src/VariaveiseTiposPrimitivos.java
@@ -2,8 +2,11 @@ public class VariaveiseTiposPrimitivos
 {
     public static void main(String[] args)
     {
+        String nome = "Daniel ";
         int idade = 46;
-        System.out.println("sua idade é: " + idade);
+
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua idade é: " + idade);
 
     }
 }
