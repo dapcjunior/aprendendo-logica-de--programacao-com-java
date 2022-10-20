8986353333fabb65612a22e038ce758ea854bdc2
dapcjunior <danielpereiracamposjunior@gmail.com>
2022-10-19 21:12:06 -0300

    Testando o comit

---
+++ src/AprendendoJava.java
@@ -0,0 +1,8 @@
+public class AprendendoJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to programming with java");
+        System.out.println("Programando  em java inicio");
+    }
+}
---
+++ src/VariaveiseTiposPrimitivos.java
@@ -0,0 +1,9 @@
+public class VariaveiseTiposPrimitivos
+{
+    public static void main(String[] args)
+    {
+        int idade = 46;
+        System.out.println("sua idade é: " + idade);
+
+    }
+}
